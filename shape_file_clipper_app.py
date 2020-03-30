18a1067aa68518efd7a39410983a8cbe04c63ebb
Thomas Zuberbuehler <thomas.zuberbuehler@gmail.com>
2020-03-29 22:14:04 -0300

    validation

--- shape_file_clipper_app.py
+++ shape_file_clipper_app.py
@@ -1,4 +1,7 @@
 import os
+import threading
+
+from abc import ABC, abstractmethod
 
 import tkinter as tk
 from tkinter import ttk, filedialog
@@ -9,24 +12,103 @@ import tk_util as tku
 import shape_file_clipper as sfc
 
 
+class Validator(ABC):
+    @abstractmethod
+    def validate(self):
+        pass
+
+
 class ShapeFileClipperApp(tk.Tk):
 
     def __init__(self):
         super().__init__()
 
-        self.title("Shape File Clipper")
-        self.geometry("980x400+100+100")
+        self.style = ttk.Style(self)
+        self.style.configure("sfc.validation.TLabel", foreground="red")
 
-        self.main_frame = MainFrame(self)
-        self.main_frame.pack(fill=tk.BOTH, expand=1)
+        self.title("Shape File Clipper")
+        self.geometry("+100+100")
+
+        self.indicator = Indicator(self, height=380, width=950)
+        self.main_frame = ShapeFileClipperAppFrame(self)
+
+        self.button_bar = ttk.Frame(self)
+        self.buttons = ttk.Frame(self.button_bar)
+
+        self.execute_button = ttk.Button(
+            self.buttons,
+            text="Execute",
+            command=self.execute
+        )
+        self.execute_button.pack(side=tk.RIGHT)
+
+        self.close_button = ttk.Button(
+            self.buttons,
+            text="Close",
+            command=self.close
+        )
+        self.close_button.pack(side=tk.RIGHT)
+
+        self.buttons.pack(side=tk.RIGHT, padx=10, pady=10)
+
+        self.indicator.grid(row=0, column=0, sticky=tku.NEW)
+        self.main_frame.grid(row=0, column=0, sticky=tku.NEW)
+        self.button_bar.grid(row=1, column=0, sticky=tku.EW)
         
         self.resizable(False, False)
 
+    def execute(self):
 
-class MainFrame(ttk.Frame):
+        messages = self.main_frame.validate()
+        if len(messages) > 0:
+            return
 
-    def __init__(self, container):
-        super().__init__(container)
+        self.__show_indicator()
+        self.execute_button["state"] = tk.DISABLED
+
+        # run in separate thread
+        threading.Thread(target=self.__execute).start()
+
+    def __show_indicator(self):
+        self.indicator.tkraise()
+
+    def __hide_indicator(self):
+        self.main_frame.tkraise()
+
+    def __execute(self):
+
+        clip_extent = self.main_frame.clip_extent_selector.get()
+        output_path = self.main_frame.output_path_selector.get()
+        output_file_name_postfix = self.main_frame.output_file_name_postfix_selector.get()
+
+        clipper = sfc.ShapeFileClipper(clip_extent, output_path, output_file_name_postfix)
+
+        shape_files = self.main_frame.shape_file_selector.get()
+        epsg_code = self.main_frame.projection_selector.get()
+
+        for shape_file in shape_files:
+            if not epsg_code or len(epsg_code.strip()) == 0:
+                clipper.clip(shape_file)
+            else:
+                clipper.clip_and_project(shape_file, epsg_code)
+
+        self.__hide_indicator()
+        self.execute_button["state"] = tk.NORMAL
+
+    def close(self):
+        self.quit()
+
+
+class Indicator(ttk.Frame):
+
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
+
+
+class ShapeFileClipperAppFrame(ttk.Frame):
+
+    def __init__(self, root, **kwargs):
+        super().__init__(root, **kwargs)
 
         self.input_frames = []
 
@@ -36,76 +118,43 @@ class MainFrame(ttk.Frame):
         self.input_frame.columnconfigure(0, weight=1)
 
         self.shape_file_selector = ShapeFileSelector(self.input_frame)
-        self.shape_file_selector.grid(row=0, column=0, padx=5, pady=5, sticky=tku.EW)
         self.input_frames.append(self.shape_file_selector)
 
         self.clip_extent_selector = ClipExtentSelector(self.input_frame)
-        self.clip_extent_selector.grid(row=1, column=0, padx=5, pady=5, sticky=tku.EW)
         self.input_frames.append(self.clip_extent_selector)
 
         self.output_path_selector = OutputPathSelector(self.input_frame)
-        self.output_path_selector.grid(row=2, column=0, padx=5, pady=5, sticky=tku.EW)
         self.input_frames.append(self.output_path_selector)
 
         self.projection_selector = ProjectionSelector(self.input_frame)
-        self.projection_selector.grid(row=3, column=0, padx=5, pady=5, sticky=tku.EW)
         self.input_frames.append(self.projection_selector)
 
         self.output_file_name_postfix_selector = OutputFileNamePostfixSelector(self.input_frame)
-        self.output_file_name_postfix_selector.grid(row=4, column=0, padx=5, pady=5, sticky=tku.EW)
         self.input_frames.append(self.output_file_name_postfix_selector)
+
+        self.shape_file_selector.grid(row=0, column=0, padx=5, pady=5, sticky=tku.EW)
+        self.clip_extent_selector.grid(row=1, column=0, padx=5, pady=5, sticky=tku.EW)
+        self.output_path_selector.grid(row=2, column=0, padx=5, pady=5, sticky=tku.EW)
+        self.projection_selector.grid(row=3, column=0, padx=5, pady=5, sticky=tku.EW)
+        self.output_file_name_postfix_selector.grid(row=4, column=0, padx=5, pady=5, sticky=tku.EW)
 
         self.input_frame.grid(row=1, column=0, padx=10, pady=10, sticky=tku.NSEW)
 
-        self.rowconfigure(2, weight=1)
-
-        self.button_bar = ttk.Frame(self)
-
-        self.execute_button = ttk.Button(
-            self.button_bar,
-            text="Execute",
-            command=self.execute
-        )
-        self.execute_button.pack(side=tk.RIGHT)
-
-        self.close_button = ttk.Button(self.button_bar, text="Close")
-        self.close_button.pack(side=tk.RIGHT)
-
-        self.button_bar.grid(row=2, column=0, padx=10, pady=10, sticky=tku.EW)
-
-    def execute(self):
-
+    def validate(self):
+        messages = []
         for input_frame in self.input_frames:
-            input_frame.validate()
-
-        clip_extent = self.clip_extent_selector.get()
-        output_path = self.output_path_selector.get()
-        output_file_name_postfix = self.output_file_name_postfix_selector.get()
-
-        clipper = sfc.ShapeFileClipper(clip_extent, output_path, output_file_name_postfix)
-
-        shape_files = self.shape_file_selector.get()
-        epsg_code = self.projection_selector.get()
-
-        for shape_file in shape_files:
-            if not epsg_code or len(epsg_code.strip()) == 0:
-                clipper.clip(shape_file)
-            else:
-                clipper.clip_and_project(shape_file, epsg_code)
+            message = input_frame.validate()
+            if message is not None:
+                messages.append(message)
+        return messages
 
 
-class ShapeFileListFrame(ttk.Frame):
+class ShapeFileSelector(ttk.Frame, Validator):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
-
-class ShapeFileSelector(ttk.Frame):
-
-    def __init__(self, container):
-        super().__init__(container)
-
-        self.list_frame = ShapeFileListFrame(self)
+        self.list_frame = ttk.Frame(self)
 
         self.scrollbar = ttk.Scrollbar(
             self.list_frame,
@@ -138,14 +187,28 @@ class ShapeFileSelector(ttk.Frame):
             command=self.__remove
         )
         self.remove_button.pack(fill=tk.X, expand=1)
+        self.clear_button = ttk.Button(
+            self.button_frame,
+            text="Clear",
+            command=self.__clear
+        )
+        self.clear_button.pack(fill=tk.X, expand=1)
 
         self.label = ttk.Label(self, text="Shape Files")
+
+        self.validation_label_value = tk.StringVar()
+        self.validation_label = ttk.Label(
+            self,
+            textvariable=self.validation_label_value,
+            style="sfc.validation.TLabel"
+        )
 
         self.columnconfigure(0, weight=1)
 
         self.label.grid(row=0, column=0, columnspan=2, padx=5, sticky=tk.W)
         self.list_frame.grid(row=1, column=0, padx=5, sticky=tku.NSW)
         self.button_frame.grid(row=1, column=1, padx=5, sticky=tku.NEW)
+        self.validation_label.grid(row=2, column=0, columnspan=2, padx=5, sticky=tk.W)
 
         self.__on_list_changed()
 
@@ -166,22 +229,34 @@ class ShapeFileSelector(ttk.Frame):
             self.list.delete(selection)
         self.__on_list_changed()
 
+    def __clear(self):
+        self.list.delete(0, tk.END)
+        self.__on_list_changed()
+
     def __on_list_changed(self):
         selection = self.list.curselection()
         remove_button_state = tk.NORMAL if selection else tk.DISABLED
         self.remove_button["state"] = remove_button_state
 
+        clear_button_state = tk.NORMAL if len(self.get()) > 0 else tk.DISABLED
+        self.clear_button["state"] = clear_button_state
+
     def get(self):
         return self.list.get(0, tk.END)
 
     def validate(self):
-        pass
+        if len(self.get()) > 0:
+            self.validation_label_value.set("")
+            return None
+        message = "No shape file selected."
+        self.validation_label_value.set(message)
+        return message
 
 
-class ClipExtentSelector(ttk.Frame):
+class ClipExtentSelector(ttk.Frame, Validator):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
         self.input_field_value = tk.StringVar()
 
@@ -192,7 +267,6 @@ class ClipExtentSelector(ttk.Frame):
             width=90,
             textvariable=self.input_field_value
         )
-        # self.input_field["state"] = tk.DISABLED
 
         self.select_button = ttk.Button(
             self,
@@ -200,11 +274,19 @@ class ClipExtentSelector(ttk.Frame):
             command=self.__select
         )
 
+        self.validation_label_value = tk.StringVar()
+        self.validation_label = ttk.Label(
+            self,
+            textvariable=self.validation_label_value,
+            style="sfc.validation.TLabel"
+        )
+
         self.columnconfigure(0, weight=1)
 
         self.label.grid(row=0, column=0, columnspan=2, padx=5, sticky=tk.W)
         self.input_field.grid(row=1, column=0, padx=5, sticky=tku.NW)
         self.select_button.grid(row=1, column=1, padx=5, sticky=tku.NEW)
+        self.validation_label.grid(row=2, column=0, columnspan=2, padx=5, sticky=tk.W)
 
     def __select(self):
         file_name = filedialog.askopenfilename(
@@ -218,18 +300,24 @@ class ClipExtentSelector(ttk.Frame):
         return self.input_field_value.get()
 
     def validate(self):
+
         clip_extent = self.get()
+
+        message = None
+
         if not clip_extent or len(clip_extent.strip()) == 0:
-            return "No clip extent selected."
+            message = "No clip extent selected."
         if not os.path.isfile(clip_extent):
-            return "Clip extent {} does not exist.".format(clip_extent)
-        return None
+            message = "Clip extent does not exist."
+
+        self.validation_label_value.set(message if message is not None else "")
+        return message
 
 
-class OutputPathSelector(ttk.Frame):
+class OutputPathSelector(ttk.Frame, Validator):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
         self.input_field_value = tk.StringVar()
 
@@ -240,7 +328,6 @@ class OutputPathSelector(ttk.Frame):
             width=90,
             textvariable=self.input_field_value
         )
-        # self.input_field["state"] = tk.DISABLED
 
         self.select_button = ttk.Button(
             self,
@@ -248,27 +335,47 @@ class OutputPathSelector(ttk.Frame):
             command=self.__select
         )
 
+        self.validation_label_value = tk.StringVar()
+        self.validation_label = ttk.Label(
+            self,
+            textvariable=self.validation_label_value,
+            style="sfc.validation.TLabel"
+        )
+
         self.columnconfigure(0, weight=1)
 
         self.label.grid(row=0, column=0, columnspan=2, padx=5, sticky=tk.W)
         self.input_field.grid(row=1, column=0, padx=5, sticky=tku.NW)
         self.select_button.grid(row=1, column=1, padx=5, sticky=tku.NEW)
+        self.validation_label.grid(row=2, column=0, columnspan=2, padx=5, sticky=tk.W)
 
     def __select(self):
         output_path_name = filedialog.askdirectory(title="Select Output Directory")
+        print(output_path_name)
         self.input_field_value.set(output_path_name)
 
     def get(self):
         return self.input_field_value.get()
 
     def validate(self):
-        pass
+
+        output_path = self.get()
+
+        message = None
+
+        if not output_path or len(output_path.strip()) == 0:
+            message = "No output directory selected."
+        if not os.path.isdir(output_path):
+            message = "Output directory does not exist."
+
+        self.validation_label_value.set(message if message is not None else "")
+        return message
 
 
-class ProjectionSelector(ttk.Frame):
+class ProjectionSelector(ttk.Frame, Validator):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
         self.input_field_value = tk.StringVar()
 
@@ -291,7 +398,7 @@ class ProjectionSelector(ttk.Frame):
 
         self.label.grid(row=0, column=0, columnspan=2, padx=5, sticky=tk.W)
         self.input_field.grid(row=1, column=0, padx=5, sticky=tku.NW)
-        self.select_button.grid(row=1, column=1, padx=5, sticky=tku.NEW)
+        # self.select_button.grid(row=1, column=1, padx=5, sticky=tku.NEW)
 
     def __find(self):
         pass
@@ -300,13 +407,13 @@ class ProjectionSelector(ttk.Frame):
         return self.input_field_value.get()
 
     def validate(self):
-        pass
+        return None
 
 
-class OutputFileNamePostfixSelector(ttk.Frame):
+class OutputFileNamePostfixSelector(ttk.Frame, Validator):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
 
         self.input_field_value = tk.StringVar()
         self.input_field_value.set("_clipped")
@@ -328,7 +435,7 @@ class OutputFileNamePostfixSelector(ttk.Frame):
         return self.input_field_value.get()
 
     def validate(self):
-        pass
+        return None
 
 
 def start():
